12d2b14bd0941b5a0ad2ce526af1d00567564c56
MarcxBC <marcusatoyebirash@gmail.com>
2026-07-09 16:45:28 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'cloudinary',
     'subfolder',
     'rest_framework',
     'blog',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
